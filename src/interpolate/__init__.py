0d4d7c28d1e5a1b0326bf1d82e2acbe5156f56c8
sitscholl <tscholl.simon@gmail.com>
2026-01-27 16:11:58 +0100

    import interpolation helpers in init

---
+++ src/interpolate/__init__.py
@@ -0,0 +1,5 @@
+from .interpolator import Interpolator
+from .vertical import VerticalModel
+from .residuals import ResidualModel
+from .regions import InterpolationRegions
+from .cv import CrossValidator
