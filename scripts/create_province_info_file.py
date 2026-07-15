a7492a104a46f2e5408359f17cb43ba6952b6379
sitscholl <tscholl.simon@gmail.com>
2026-07-15 10:50:00 +0200

    add script to create csv with info for province stations from meteo handler

---
+++ scripts/create_province_info_file.py
@@ -0,0 +1,20 @@
+import asyncio
+import pandas as pd
+
+from src.meteo import BaseMeteoHandler
+
+def main():
+    meteo_loader = BaseMeteoHandler.create("province_api", target_timezone = "Europe/Rome")
+
+    async def load():
+        async with meteo_loader as loader: 
+            st_info = await loader.get_station_info()
+        return st_info
+
+    st_info = asyncio.run(load())
+    return pd.DataFrame.from_dict(st_info, orient = 'index')
+
+if __name__ == '__main__':
+
+    st_info = main()
+    st_info.rename(columns = {'id': 'station_id'}).to_csv('data/province/stations.csv', index = False)
